308a9f108050ab85663f1e9d177fa35c18f5a7dd
alexeVal <62025563+alexeVal@users.noreply.github.com>
2020-04-09 18:25:49 +0300

    обавлен аларм менеджер

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
     }
 }
